db28251ef78a89247f3cd11154b3663a68fb0e1b
anishmoktan <anish.tmngo@gmail.com>
2021-02-07 16:00:28 -0500

    name change?

--- first_code.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    profile = "default"
-    region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "tf_course" {
-    bucket = "anish-2021"
-    acl = "private"
-}
--- prod.tf
+++ prod.tf
@@ -3,6 +3,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
+
 resource "aws_s3_bucket" "tf_course" {
     bucket = "anish-2021"
     acl = "private"
