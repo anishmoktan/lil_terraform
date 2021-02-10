2c1e78a2bee27f53dab40bed25b4d777778c6a13
anishmoktan <anish.tmngo@gmail.com>
2021-02-10 18:38:47 -0500

    changed

--- prod.tf
+++ prod.tf
@@ -1,14 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.27.0"
+    }
+  }
+}
+
 provider "aws" {
-    profile = "default"
-    region = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_course" {
-    bucket = "anish-2021"
-    acl = "private"
-}
-
-resource "aws_s3_bucket" "tf_course2" {
-    bucket = "anish-2021A"
-    acl = "private"
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 }
