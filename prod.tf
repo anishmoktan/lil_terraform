f822b70a20853ddc0850b5b0e3e416829f9f0943
anishmoktan <anish.tmngo@gmail.com>
2021-02-10 20:30:52 -0500

    final

--- prod.tf
+++ prod.tf
@@ -1,10 +1,10 @@
 terraform {
- backend "remote" {
-   organization = "anishmoktan"
-   workspaces {
-     name = "Example-Workspace"
-   }
- }
+#  backend "remote" {
+#    organization = "anishmoktan"
+#    workspaces {
+#      name = "Example-Workspace"
+#    }
+#  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
